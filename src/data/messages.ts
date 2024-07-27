77c9392766eaa2eecd74f939f99e184f7a6ed041
asad2103 <asadwarsame11@gmail.com>
2024-07-27 07:05:59 -0400

    maulid

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -3,6 +3,7 @@ export interface Message {
   subject: string;
   date: string;
   id: number;
+  img: string;
 }
 
 const messages: Message[] = [
@@ -10,50 +11,58 @@ const messages: Message[] = [
     fromName: 'Matt Chorsey',
     subject: 'New event: Trip to Vegas',
     date: '9:32 AM',
-    id: 0
+    id: 0,
+    img:"hello4.avif"
   },
   {
     fromName: 'Lauren Ruthford',
     subject: 'Long time no chat',
     date: '6:12 AM',
-    id: 1
+    id: 1,
+    img:"hello6.avif"
   },
   {
     fromName: 'Jordan Firth',
     subject: 'Report Results',
     date: '4:55 AM',
-    id: 2
+    id: 2,
+    img:"hello5.avif"
 
   },
   {
     fromName: 'Bill Thomas',
     subject: 'The situation',
     date: 'Yesterday',
-    id: 3
+    id: 3,
+    img:"hello8.avif"
   },
   {
     fromName: 'Joanne Pollan',
     subject: 'Updated invitation: Swim lessons',
     date: 'Yesterday',
-    id: 4
+    id: 4,
+    img:"hello7.avif"
   },
   {
     fromName: 'Andrea Cornerston',
     subject: 'Last minute ask',
     date: 'Yesterday',
-    id: 5
+    id: 5,
+    img:"hello9.webp"
   },
   {
     fromName: 'Moe Chamont',
     subject: 'Family Calendar - Version 1',
     date: 'Last Week',
-    id: 6
+    id: 6,
+    img:"hello10.avif"
   },
   {
     fromName: 'Kelly Richardson',
     subject: 'Placeholder Headhots',
     date: 'Last Week',
-    id: 7
+    id: 7,
+    img:"hello11.avif"
   }
 ];
 
